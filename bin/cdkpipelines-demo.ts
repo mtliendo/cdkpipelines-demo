2657643e244f78293f296230c4f4c68b503c70b9
Michael Liendo <mtliendo@amazon.com>
2021-11-17 23:47:41 -0600

    update pipeline info

--- bin/cdkpipelines-demo.ts
+++ bin/cdkpipelines-demo.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 import 'source-map-support/register'
 import * as cdk from '@aws-cdk/core'
-import { CdkpipelinesDemoStack } from '../lib/cdkpipelines-demo-stack'
+import { CdkpipelinesDemoPipelineStack } from '../lib/cdkpipelines-demo-pipeline-stack'
 
 const app = new cdk.App()
-new CdkpipelinesDemoStack(app, 'CdkpipelinesDemoStack', {
+new CdkpipelinesDemoPipelineStack(app, 'CdkpipelinesDemoPipelineStack', {
 	/* If you don't specify 'env', this stack will be environment-agnostic.
 	 * Account/Region-dependent features and context lookups will not work,
 	 * but a single synthesized template can be deployed anywhere. */
--- lib/cdkpipelines-demo-pipeline-stack.ts
+++ lib/cdkpipelines-demo-pipeline-stack.ts
@@ -15,7 +15,9 @@ export class CdkpipelinesDemoPipelineStack extends Stack {
 			// How it will be built and synthesized
 			synth: new ShellStep('Synth', {
 				// Where the source can be found
-				input: CodePipelineSource.gitHub('mtliendo/cdk-export-sample', 'main'),
+				input: CodePipelineSource.gitHub('mtliendo/cdk-export-sample', 'main', {
+					authentication: SecretValue.secretsManager('cdk-token'),
+				}),
 
 				// Install dependencies, build and run cdk synth
 				commands: ['npm ci', 'npm run build', 'npx cdk synth'],
